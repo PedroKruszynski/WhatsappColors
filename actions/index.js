7c3b8389c872f45213fc8044617f25c760f54a27
Pedro Kruszynski <pedro.nascimento39@etec.sp.gov.br>
2022-02-13 23:26:01 -0300

    more variables

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,9 @@ const getElementById = (idOfElement) => document.getElementById(idOfElement);
 
 const getAllVariables = () => ({
   primaryButton: '--button-primary-background',
+  primaryButtonHover: '--button-primary-background-hover',
+  buttonSecondaryLetter: '--button-secondary',
+  buttonSecondaryLetterHover: '--button-secondary-hover',
   backgroundTop: '--app-background-stripe',
   progressBeginWhatsapp: '--progress-primary',
   circleUnreadMesssage: '--unread-marker-background',
@@ -9,7 +12,19 @@ const getAllVariables = () => ({
   bottomLine: '--intro-border',
   statusToView: '--teal',
   notListened: '--ptt-green',
-  iconSearchBack: '--icon-search-back'
+  iconSearchBack: '--icon-search-back',
+  viewedMessage: '--icon-ack',
+  decriptionGroup: '--input-empty-value-placeholder',
+  userIsAdmin: '--chat-marker-admin',
+  highlight: '--highlight',
+  swithButtonChecked: '--switch-button-checked-color',
+  checkboxColor: '--checkbox-background',
+  iconRound: '--round-icon-background',
+  backgroundPanelColor: '--panel-background-colored',
+  backgroundPanel: '--panel-background-colored-deeper',
+  audioListened: '--audio-progress-played-outgoing',
+  securityIconLock: '--security-icon-lock',
+  inputActiveBottom: '--input-border-active' 
 });
 
 const getAllSvgs = () => ({
@@ -23,7 +38,7 @@ const changeColor = () => {
   const rootElement = document.documentElement;
 
   Object.entries(allVariables).forEach(([index, variableName]) => {
-    rootElement.style.setProperty(variableName, '#FA6FF9');
+    rootElement.style.setProperty(variableName, '#3D49FA');
   });
 
   // console.log(document.getElementById('df9d3429-f0ef-48b5-b5eb-f9d27b2deba6').childNodes[1])
